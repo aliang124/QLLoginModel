3267c92356ad402437890b777427706ce1af7c57
计恩良 <454309007@qq.com>
2018-12-23 03:25:55 +0800

    1
    1

--- QLLoginModel/QLWanShanViewController.m
+++ QLLoginModel/QLWanShanViewController.m
@@ -13,7 +13,7 @@
 #import "QLWanShanTitleCell.h"
 #import "QLNickNameCell.h"
 #import "QLSelectCheckCell.h"
-
+#import "QLChildInfoCell.h"
 @interface QLWanShanViewController ()
 @end
 
@@ -37,6 +37,7 @@
     self.formManager[@"QLWanShanTitleItem"] = @"QLWanShanTitleCell";
     self.formManager[@"QLNickNameItem"] = @"QLNickNameCell";
     self.formManager[@"QLSelectCheckItem"] = @"QLSelectCheckCell";
+    self.formManager[@"QLChildInfoItem"] = @"QLChildInfoCell";
     [self initForm];
 }
 
@@ -67,44 +68,13 @@
     itShenFen.rightText = @"宝爸";
     [section0 addItem:itShenFen];
 
+    QLChildInfoItem *info = [[QLChildInfoItem alloc] init];
+    [section0 addItem:info];
+    
     [sectionArray addObject:section0];
     [self.formManager replaceSectionsWithSectionsFromArray:sectionArray];
     
     [self.formTable reloadData];
 }
 
-- (void)createSubView {
-    //孩子信息
-//    UILabel *haiInfo = [[UILabel alloc] initWithFrame:CGRectMake(24, _checkShenFenBtn.bottom+12, WTScreenWidth-24-23, 12)];
-//    haiInfo.font = WTFontSys(12);
-//    haiInfo.textColor = QL_DescColor_Gray;
-//    haiInfo.text = @"孩子信息";
-//    [self.view addSubview:haiInfo];
-//    
-//    UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(24, haiInfo.bottom+12, WTScreenWidth-24-23, 184)];
-//    bgView.layer.cornerRadius = 2;
-//    bgView.layer.masksToBounds = YES;
-//    bgView.layer.borderColor = WTColorHex(0xECECE6).CGColor;
-//    bgView.layer.borderWidth = 1;
-//    [self.view addSubview:bgView];
-//    
-//    UIImageView *line1 = [[UIImageView alloc] initWithFrame:CGRectMake(12, 48, bgView.width-12-12, 1)];
-//    line1.backgroundColor = WTColorHex(0xEEEEE5);
-//    [bgView addSubview:line1];
-//
-//    UIImageView *line2 = [[UIImageView alloc] initWithFrame:CGRectMake(12, 96, bgView.width-12-12, 1)];
-//    line2.backgroundColor = WTColorHex(0xEEEEE5);
-//    [bgView addSubview:line2];
-//
-//    UIImageView *line3 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 144, bgView.width, 1)];
-//    line3.backgroundColor = WTColorHex(0xEEEEE5);
-//    [bgView addSubview:line3];
-//    //清除信息
-//    UIButton *clearInfo = [[UIButton alloc] initWithFrame:CGRectMake(0, 145, bgView.width, 39)];
-//    [clearInfo setBackgroundImage:[WTUtil createImageFromColor:[UIColor whiteColor]] forState:UIControlStateNormal];
-//    clearInfo.titleLabel.font = WTFontSys(12);
-//    [clearInfo setTitleColor:WTColorHex(0xFF6A18) forState:UIControlStateNormal];
-//    [clearInfo setTitle:@"清除信息" forState:UIControlStateNormal];
-//    [bgView addSubview:clearInfo];
-}
 @end
